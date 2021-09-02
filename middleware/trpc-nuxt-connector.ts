8dd4306353a73c7b69d72493f97abb3dd62a176e
Amos Manneschmidt <amoose136@gmail.com>
2021-09-02 08:05:59 -0400

    temp comment out middleware/plugin

--- middleware/trpc-nuxt-connector.ts
+++ middleware/trpc-nuxt-connector.ts
@@ -1,33 +1,33 @@
-import { defineNuxtMiddleware } from '@nuxtjs/composition-api'
-import * as trpc from '@trpc/server'
-import { color, PrismaClient } from '@prisma/client'
-import { z } from 'zod'
+// import { defineNuxtMiddleware } from '@nuxtjs/composition-api'
+// import * as trpc from '@trpc/server'
+// import { color, PrismaClient } from '@prisma/client'
+// import { z } from 'zod'
 
-const prisma = new PrismaClient();
+// const prisma = new PrismaClient();
 
-export default defineNuxtMiddleware( ctx => {
-    // do stuff
-    const allColors = createRouter()
-    .query('list', {
-        resolve(){
-            const colors: color[] = await prisma.color.findMany()
-            return colors;
-        }
-    });
+// export default defineNuxtMiddleware( ctx => {
+//     // do stuff
+//     const allColors = createRouter()
+//     .query('list', {
+//         resolve(){
+//             const colors: color[] = await prisma.color.findMany()
+//             return colors;
+//         }
+//     });
 
-    const 
-    export const apiRouter = trpc.router<Context>(ctx).query('hello', {
-        input: z
-          .object({
-            text: z.string().nullish(),
-          })
-          .nullish(),
-        resolve({ input }) {
-          return {
-            greeting: `hello ${input?.text ?? 'world'}`,
-          };
-        },
-      });
-    
-    export type AppRouter = typeof apiRouter;
-}
+//     const
+//     export const apiRouter = trpc.router<Context>(ctx).query('hello', {
+//         input: z
+//           .object({
+//             text: z.string().nullish(),
+//           })
+//           .nullish(),
+//         resolve({ input }) {
+//           return {
+//             greeting: `hello ${input?.text ?? 'world'}`,
+//           };
+//         },
+//       });
+
+//     export type AppRouter = typeof apiRouter;
+// }
