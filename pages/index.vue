e3709180dbba20a78c920bd0c7eb798f7f7e287d
Amos Manneschmidt <amoose136@gmail.com>
2021-09-02 08:06:30 -0400

    Make demo single page application that shows mock data

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,44 @@
 <template>
-    <p>hello world</p>
+    <div :style="{ 'background-color': ccolor }">
+        <p>List of colors</p>
+        <ul>
+            <li
+                v-for="color in colors"
+                :key="color"
+                @click="
+                    {
+                        ccolor = '#' + color.color
+                    }
+                "
+            >
+                {{ color.name }}
+            </li>
+        </ul>
+    </div>
 </template>
 
 <script lang="ts">
+// this should SSR render all color names as an unordered list
 import Vue from 'vue'
+import { PrismaClient } from '@prisma/client'
 
-export default Vue.extend({})
+export default Vue.extend({
+    async asyncData() {
+        const prisma = new PrismaClient()
+        const colors = await prisma.color.findMany({
+            select: {
+                color: true,
+                id: false,
+                name: true,
+                createdAt: false,
+            },
+        })
+        return { colors }
+    },
+    data() {
+        return {
+            ccolor: 'white',
+        }
+    },
+})
 </script>
